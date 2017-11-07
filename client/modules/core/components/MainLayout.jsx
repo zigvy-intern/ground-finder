32056348bffba810557c760a4d51d2578cb6375a
thientran511 <32450660+thientran511@users.noreply.github.com>
2017-11-07 17:45:19 +0700

    change file

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import {PageHeader , Button, Row, Col, Navbar, NavItem, Nav,DropdownButton,MenuItem} from 'react-bootstrap';
+
 const Layout = ({content}) => (
-<div>
-<header>
-<h1>Impossible List </h1>
-</header>
-<div>
-{content()}
-</div>
-</div>
+  <Navbar>
+    <Nav>
+      <NavItem eventKey={1} href="#">FIND MATCH</NavItem>
+      <NavItem eventKey={2} href="#">GROUNDS</NavItem>
+      <NavItem eventKey={3} href="#">MESSAGES</NavItem>
+      <NavItem eventKey={4} href="#">UPCOMING</NavItem>
+    </Nav>
+    <DropdownButton bsStyle={title.toLowerCase()} title={title} key={i} id={`dropdown-basic-${i}`}>
+      <MenuItem eventKey="1">Action</MenuItem>
+      <MenuItem eventKey="2">Another action</MenuItem>
+      <MenuItem eventKey="3" active>Active Item</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey="4">Separated link</MenuItem>
+    </DropdownButton>
+  );
+}
+  </Navbar>
 );
-export default Layout
+
+
+
+
+export default Layout;
