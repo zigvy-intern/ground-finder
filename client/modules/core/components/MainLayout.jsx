050dce8334271d24f76b236feac8ebac1f44b5be
Khanh <khanhgia725@gmail.com>
2017-11-07 17:38:43 +0700

    create menu

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {PageHeader , Button, Row, Col, Navbar, NavItem, Nav,NavDropdown,MenuItem,FormGroup,FormControl,Glyphicon} from 'react-bootstrap';
+
 const Layout = ({content}) => (
-<div>
-<header>
-<h1>Impossible List </h1>
-</header>
-<div>
-{content()}
-</div>
-</div>
+  <Navbar>
+  <Nav>
+    <Navbar.Form pullLeft>
+      <FormGroup>
+        <FormControl type="text"  placeholder="Search, grounds,teams or someone..." />
+      </FormGroup>
+      {' '}
+      <Button type="submit" ><Glyphicon glyph= "search"></Glyphicon></Button>
+      </Navbar.Form>
+      <NavItem eventKey={1} href="#">FIND MATCH</NavItem>
+      <NavItem eventKey={2} href="#">GROUNDS</NavItem>
+      <NavItem eventKey={3} href="#">MESSAGES</NavItem>
+      <NavItem eventKey={4} href="#">UPCOMING</NavItem>
+      <NavDropdown>
+      <MenuItem eventKey={1} href="#">Login</MenuItem>
+      <MenuItem eventKey={1} href="#">Logout </MenuItem>
+      </NavDropdown>
+  </Nav>
+  </Navbar>
 );
 export default Layout
