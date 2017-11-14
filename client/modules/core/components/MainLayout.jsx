067dd360c3a0d8a0a4895d02cf420759b0e2536d
Khanh <khanhgia725@gmail.com>
2017-11-14 14:13:46 +0700

    update menu and footer

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -6,7 +6,7 @@ const Layout = ({content}) => (
         <Navbar inverse style={{backgroundColor:"black"}} collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#"><img src= "https://i.imgur.com/RqMQ1gI.png" height="29" width="auto"/></a>
+              <a href="#"><img src= "https://i.imgur.com/0hzaXxK.png" height="29" width="auto"/></a>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
@@ -36,8 +36,34 @@ const Layout = ({content}) => (
           </Navbar.Collapse>
         </Navbar>
         {content()}
-    </div>
 
+        <Navbar >
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#"><img src= "https://i.imgur.com/TZ96sKM.png" height="29" width="auto"/></a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Nav pullLeft>
+            <NavItem eventKey={1} href="#">Download App</NavItem>
+            <NavItem eventKey={2} href="#">F.A.Q</NavItem>
+            <NavItem eventKey={3} href="#">Support</NavItem>
+            <NavItem eventKey={4} href="#">Feedback</NavItem>
+            <br> ©2017 Zigvy Inc. </br>
+          </Nav>
+          <Nav pullRight>
+            <Navbar.Brand>
+            <a href="#"><img src= "https://i.imgur.com/8QPN2zZ.png" height="16" width="16"/></a>
+            {' '}
+            <a href="#"><img src= "https://i.imgur.com/sFIaNFm.png" height="16" width="16"/></a>
+            {' '}
+            <a href="#"><img src= "https://i.imgur.com/T4T7fs3.png" height="16" width="16"/></a>
+        </Navbar.Brand>
+
+          </Nav>
+
+        </Navbar>
+  </div>
 
 );
 export default Layout
--- client/modules/items/components/ItemList.jsx
+++ client/modules/items/components/ItemList.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import {Button, Row, Col, Navbar, NavItem, Nav, NavDropdown, MenuItem, FormGroup, FormControl, Glyphicon, PageFooter, ResponsiveEmbed, Grid} from 'react-bootstrap';
-import FontAwesome from 'react-fontawesome';
+const ItemList = ({content = () => null }) => (
+<div>
+<p></p>
 
-const ItemList = ({content}) => (
-<Navbar >
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#"><img src= "https://i.imgur.com/TZ96sKM.png" height="29" width="auto"/></a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Nav pullLeft>
-    <NavItem eventKey={1} href="#">Download App</NavItem>
-    <NavItem eventKey={2} href="#">F.A.Q</NavItem>
-    <NavItem eventKey={3} href="#">Support</NavItem>
-    <NavItem eventKey={4} href="#">Feedback</NavItem>
-    <br> ©2017 Zigvy Inc. </br>
-  </Nav>
-</Navbar>
+</div>
 );
-
 export default ItemList;
