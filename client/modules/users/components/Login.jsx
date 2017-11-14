346d1da1feb76e9aefe4e6d41e421e6cccfef861
thientran511 <32450660+thientran511@users.noreply.github.com>
2017-11-14 14:21:38 +0700

    repair login

--- client/modules/users/components/Login.jsx
+++ client/modules/users/components/Login.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { Col, Panel, Input, Button, Glyphicon, FormGroup, FormControl } from 'react-bootstrap';
+import { Col, Panel, Input,FormGroup, FormControl, Button, Glyphicon } from 'react-bootstrap';
 
-class NewUser extends React.Component {
-  render() {
-      const {error} = this.props;
-      return (
-        <Col xs={12} sm={6} smOffset={3}>
-          <Panel>
-            <h1>Register</h1>
-            {error ? <p style={{color: 'red'}}>{error}</p> : null}
-            <form>
-              <FormGroup>
-              <FormControl inputRef={mail => this.refMail = mail} type="email" placeholder="Email" />
-              <FormControl inputRef={password => this.refPass = password} type="password" placeholder="Password" />
-              <Button onClick={this.createUser.bind(this)}
-              bsStyle="primary" type="submit" >Sign up </Button>
-              </FormGroup>
-            </form>
-          </Panel>
-        </Col>
-      )
-    }
+  class Login extends React.Component {
+      render() {
+        const {error} = this.props;
+          return (
+            <Col xs={12} sm={6} smOffset={3}>
+              <Panel>
+                <h1>Login</h1>
+                  {error ? <p style={{color: 'red'}}>{error}</p> : null}
+                    <form>
+                      <FormGroup>
+                        <FormControl inputRef={mail => this.refMail = mail} type="email" placeholder="Email" />
+                        <FormControl inputRef={password => this.refPass = password} type="password" placeholder="Password" />
+                        <Button onClick={this.login.bind(this)}
+                        bsStyle="primary" type="submit" >Login </Button>
+                      </FormGroup>
+                    </form>
+                  </Panel>
+                </Col>
+              )
+            }
 
-    createUser(e) {
-      e.preventDefault();
-      const {createUser} = this.props;
-      const mail = this.refMail.value;
-      const password = this.refPass.value;
-      createUser(mail,password);
-    }
-}
-export default NewUser
+        login(e) {
+            e.preventDefault();
+            const {loginUser} = this.props;
+            const mail = this.refMail.value;
+            const password = this.refPass.value;
+            loginUser(mail,password);
+          }
+        }
+export default Login;
--- client/modules/users/containers/Login.js
+++ client/modules/users/containers/Login.js
@@ -1,5 +1,6 @@
 import Login from '../components/Login.jsx';
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
+
 export const composer = ({context, clearErrors}, onData) => {
 const {LocalState} = context();
 const error = LocalState.get('LOGIN_ERROR');
@@ -7,6 +8,7 @@ onData(null, {error});
 
 return clearErrors;
 };
+
 export const depsMapper = (context, actions) => ({
   loginUser: actions.users.login,
   clearErrors: actions. users. clearErrors,
